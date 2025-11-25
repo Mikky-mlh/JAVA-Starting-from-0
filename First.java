174f257808ff6d3a7a34f89ef5d5af500a905d20
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-11-25 21:26:17 +0530

    1First

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int age;
+        age = 20;
+        System.out.println("My age is " + age);
+    }
+}
