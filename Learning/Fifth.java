964a9547ef3101c8f2bdd44e43fe0607c2451145
Yuvraj Sarathe <yuvrajsarathe07@gmail.com>
2025-11-26 19:42:26 +0530

    fifth

---
+++ Learning/Fifth.java
@@ -0,0 +1,74 @@
+//Todo: IF-ELSE STATEMENT
+
+import java.util.Scanner;
+
+public class Fifth 
+{
+    public static void main(String[] args) 
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18)
+        {
+            System.out.println("You are eligible to vote.");
+        }
+        else if(age<18) {
+            System.out.println("You are not eligible to vote.");
+        }
+        else
+        {
+            System.out.println("Invalid age.");
+        }
+
+        System.out.print("Enter the time in hours: ");
+        int time = scanner.nextInt();
+        
+
+        System.out.println((time < 18) ? "Good day." : "Good evening.");
+        //& Ternary operator: (condition) ? value if true : value if false
+
+        String message = (time < 12) ? "Good morning."
+                        : (time < 18) ? "Good day."
+                        : "Good evening.";
+        System.out.println(message);
+
+
+        int  a = 200;
+        int b = 300;
+        int c = 400;
+        int x = 500;
+        int y = 600;
+        int z = 550;
+        if (a>b && a>c) // AND opeartor
+        {
+            System.out.println("A is the largest number.");
+        }
+        else if(b>a && b>c)
+        {
+            System.out.println("B is the largest number.");
+        }
+        else
+        {
+            System.out.println("C is the largest number.");
+        }
+
+        if (x>y || x>z) {
+            System.out.println("OR operator atleast one is true.");
+        }
+        else{
+            System.out.println("OR operator both are false.");
+        }
+
+        int d = 33;
+        int e = 200;
+
+        if (!(d > e)) {
+        System.out.println("d is NOT greater than e");
+        } // not operator
+
+        scanner.close();
+    }
+}
